275c3f1839b5eb41fee3f42d7a2657b47ccd35cd
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-02-27 15:25:25 -0700

    part 1 files

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  };
+}
