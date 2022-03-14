3d9c74ea28a1057af1d17c8fc7b97681df459cee
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-03-13 22:00:14 -0600

    part 2 files

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+
+  eleventyConfig.addWatchTarget("src/css/");
+
   return {
     dir: {
       input: 'src',
